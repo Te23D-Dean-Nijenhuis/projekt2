ede885780a2c14cb1f9efcd0c1b97d09f91a9b5c
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2026-05-11 10:49:54 +0200

    kopierade över inläsnings metoder och ändrade lite på menyn

--- src/main/java/librarysystem/Main.java
+++ src/main/java/librarysystem/Main.java
@@ -1,19 +1,48 @@
 package librarysystem;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         menu();
         
     }
+
     public static void menu() {
-        System.out.println("=========================================");
-        System.out.println("|Meny:                                  |");
-        System.out.println("|Val 1: Hämta böcker.                   |");
-        System.out.println("|Val 2: Hämta tidningar.                |");
-        System.out.println("|Val 3: Skriv ut hämtade artiklar.      |");
-        System.out.println("|Val 4: Lägg till en bok i systemet.    |");
-        System.out.println("|Val 5: Lägg till en tidning i systemet.|");
-        System.out.println("|val 6: Avsluta programmet .            |");
-        System.out.println("========================================="); 
+        System.out.println("==========================================");
+        System.out.println("| Meny:                                  |");
+        System.out.println("| Val 1: Hämta böcker.                   |");
+        System.out.println("| Val 2: Hämta tidningar.                |");
+        System.out.println("| Val 3: Skriv ut hämtade artiklar.      |");
+        System.out.println("| Val 4: Lägg till en bok i systemet.    |");
+        System.out.println("| Val 5: Lägg till en tidning i systemet.|");
+        System.out.println("| Val 6: Avsluta programmet.             |");
+        System.out.println("| Skriv 1-6.                             |");;
+        System.out.println("==========================================");
+    }
+
+    public static String readString(Scanner keyboard) { // gör som namnet heter
+        String temp = "sfjsifj"; // blev ibland problem när stringen var nullad
+
+        temp = keyboard.next();
+        return temp;
+    }
+
+    public static int readInt(Scanner keyboard) { // gör som namnet heter
+        int temp = 0;
+        boolean success = false;
+        while (!success) {
+            try {
+                temp = keyboard.nextInt();
+                success = true;
+            } catch (Exception e) {
+                System.out.println("Det får endast vara en int!");
+                success = false;
+                keyboard.nextLine();
+            }
+        }
+        keyboard.nextLine();
+        return temp;
+
     }
 }
